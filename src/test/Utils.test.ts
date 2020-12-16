71a3ef21e2179fcfd5c34f741228aeea6c4962de
lloydnoone <lloydnoone92@gmail.com>
2020-12-16 17:21:23 +0000

    add hooks and launch.json for debugging

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,14 @@
 import { Utils } from '../app/Utils'
 
 describe('Utils test suite', () => {
+  beforeEach(() => {
+    console.log('before each')
+  })
+
+  beforeAll(() => {
+    console.log('before all')
+  })
+  
   test('first test', () => {
     const result = Utils.toUpperCase('abc')
     expect(result).toBe('ABC')
@@ -15,12 +23,35 @@ describe('Utils test suite', () => {
   })
 
   test('parse URL with query', () => {
-    const parsedUrl = Utils.parseUrl('http:localhost:8080/login?user=user&password=pass')
+    const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass')
     const expectedQuery = {
       user: 'user',
       password: 'pass'
     }
     expect(parsedUrl.query).toEqual(expectedQuery)
     expect(expectedQuery).toBe(expectedQuery)
+  })
+
+  test('test invalid URL', () => {
+    function expectError() {
+      Utils.parseUrl('')
+    }
+    expect(expectError).toThrowError('Empty url')
+  })
+
+  test('test invalid url inside an arrow function', () => {
+
+    expect(() => {
+      Utils.parseUrl('')
+    }).toThrow('Empty url')
+  })
+
+  test('test invalid URL with try catch', () => {
+    try {
+      Utils.parseUrl('')
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error)
+      expect(error).toHaveProperty('message', 'Empty url')
+    }
   })
 })
